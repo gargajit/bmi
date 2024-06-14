ee750be0ae4f0c9032885731242ea3f240907c82
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-06-15 00:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,26 @@ return (weight / Math.pow(height, 2));
 // creates a new paragraph to display the result
 function createPara(bmi) {
     const newPara = document.createElement("p");
-    if (bmi <= 18.4){newPara.id = "result_underweight";}
-    else if (bmi >= 18.5 && bmi <= 24.9){newPara.id = "result_normal";}
-    else if (bmi >= 25 && bmi <= 39.9){newPara.id = "result_overweight";}
-    else {newPara.id = "result_obese"}
+    if (bmi <= 18.4){
+        newPara.id = "result_underweight";
+        const highlightRow1 = document.getElementById("row1");
+        highlightRow1.style.color = "tomato";
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9){
+        newPara.id = "result_normal";
+        const highlightRow2 = document.getElementById("row2");
+        highlightRow2.style.color = "#5cb800";
+    }
+    else if (bmi >= 25 && bmi <= 39.9){
+        newPara.id = "result_overweight"; 
+        const highlightRow3 = document.getElementById("row3");
+        highlightRow3.style.color = "#e39924";
+    }
+    else {
+        newPara.id = "result_obese";
+        const highlightRow4 = document.getElementById("row4");
+        highlightRow4.style.color = "tomato";
+    }
     
     newPara.innerHTML = "BMI: " + bmi;
     document.getElementById('input_box').appendChild(newPara);
